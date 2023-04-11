ede85db9baf5846579307080ca4ab7c2e814c602
ashomurodov <ayyuubshomurodov@gmail.com>
2023-04-11 23:34:25 +0500

    make good for phones

--- main.js
+++ main.js
@@ -14,54 +14,45 @@ function handleClick(element) {
 function createBox([topR, leftR, topNumber1, leftNumber1]) {
   let counter = 0;
   let div = document.createElement("div");
-  let _randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  let _randomColor = randomColor();
+
   div.style.backgroundColor = `#${_randomColor}`;
   div.classList.add("box");
   div.innerText = counter;
   div.addEventListener("click", () => handleClick(div));
   document.body.appendChild(div);
+
   return setInterval(() => {
     leftR += leftNumber1;
     topR += topNumber1;
 
     div.style.left = `${leftR}px`;
     div.style.top = `${topR}px`;
-    if (topR >= window.innerHeight - 125) {
-      topNumber1 = topNumber1 * -1;
+
+    const makeShorter = (arg) => {
+      arg == "top" ? (topNumber1 = topNumber1 * -1) : (leftNumber1 = leftNumber1 * -1);
       _randomColor = randomColor();
       div.style.backgroundColor = `#${_randomColor}`;
       div.style.boxShadow = `-1px -2px 170px 24px #${_randomColor}75`;
       counter++;
       div.innerText = counter;
-    } else if (leftR >= window.innerWidth - 120) {
-      leftNumber1 = leftNumber1 * -1;
-      _randomColor = randomColor();
-      div.style.backgroundColor = `#${_randomColor}`;
-      div.style.boxShadow = `-1px -2px 170px 24px #${_randomColor}75`;
-      counter++;
-      div.innerText = counter;
-    } else if (topR <= 0) {
-      topNumber1 = topNumber1 * -1;
-      _randomColor = randomColor();
-      div.style.backgroundColor = `#${_randomColor}`;
-      div.style.boxShadow = `-1px -2px 170px 24px #${_randomColor}75`;
-      counter++;
-      div.innerText = counter;
-    } else if (leftR <= 0) {
-      leftNumber1 = leftNumber1 * -1;
-      _randomColor = randomColor();
-      div.style.backgroundColor = `#${_randomColor}`;
-      div.style.boxShadow = `-1px -2px 170px 24px #${_randomColor}75`;
-      counter++;
-      div.innerText = counter;
+      console.log("hello");
+    };
+
+    if (topR >= window.innerHeight - 125 || topR <= 0) {
+      // <---> hit top
+      makeShorter("top");
+    } else if (leftR >= window.innerWidth - 120 || leftR <= 0) {
+      // <---> by left
+      makeShorter("left");
     }
   }, 15);
 }
 
 const newElement = createBox([400, 23, -3, -2]);
-const newElement1 = createBox([300, 663, -5, -2]);
+const newElement1 = createBox([300, 263, -5, -2]);
 const newElement2 = createBox([70, 242, -3, -2]);
-const newElement3 = createBox([4, 900, 3, 3]);
+const newElement3 = createBox([4, 300, 3, 3]);
 const newElement4 = createBox([340, 143, 4, -3]);
 
 // console.log(window.innerWidth, window.innerHeight);
