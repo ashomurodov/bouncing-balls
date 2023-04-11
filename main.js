f7fe65126684869580d1bc90e3c63dd293df0b48
ashomurodov <ayyuubshomurodov@gmail.com>
2023-04-11 14:46:54 +0500

    fix: delete trash codes

--- main.js
+++ main.js
@@ -1,38 +1,28 @@
-const dvd = document.querySelector(".dvd-container");
-const img = document.querySelector(".img");
+const dvd = document.querySelector(".dvd-container"), img = document.querySelector(".img");
 
-let leftE = 0;
-let topE = 0;
-let topNumber = 2;
-let leftNumber = 3;
-let counter = 0;
+let leftE = 0, topE = 0, topNumber = 2, leftNumber = 3, counter = 0;
 setInterval(() => {
   leftE += leftNumber;
   topE += topNumber;
   dvd.style.left = `${leftE}px`;
   dvd.style.top = `${topE}px`;
   if (topE >= window.innerHeight - 137) {
-    console.log("pastga urildi");
     topNumber = -2;
     counter++;
     img.src = `dvdlogo-0${counter}.svg`;
   } else if (leftE >= window.innerWidth - 300) {
-    console.log("chapga urild");
     leftNumber = -3;
     counter++;
     img.src = `dvdlogo-0${counter}.svg`;
   } else if (topE <= 0) {
-    console.log("tepaga urildi");
     topNumber = 2;
     counter++;
     img.src = `dvdlogo-0${counter}.svg`;
   } else if (leftE <= 0) {
-    console.log("chapga urildi");
     leftNumber = 3;
     counter++;
     img.src = `dvdlogo-0${counter}.svg`;
   }
-
   if (counter == 7) counter = 0;
 }, 15);
 
