fc1430914c51343ac01b23ba384926e2e4896444
ashomurodov <ayyuubshomurodov@gmail.com>
2023-04-11 17:30:18 +0500

    feat: cleaner cod

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const dvd = document.querySelectorAll(".dvd-container"),
   img = document.querySelectorAll(".img");
 
-function createInterval(topE, leftE, topNumber, leftNumber) {
-  let leftR = leftE;
-  let topR = topE;
-  let leftNumber1 = leftNumber;
-  let topNumber1 = topNumber;
+function randomColor() {
+  let color = Math.floor(Math.random() * 16777215).toString(16);
+  console.log(color);
+  return color;
+}
+
+function createBox(...[topR, leftR, topNumber1, leftNumber1]) {
+
   let div = document.createElement("div");
-  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  let _randomColor = Math.floor(Math.random() * 16777215).toString(16);
   div.style.backgroundColor = `#${randomColor}`;
   div.classList.add("box");
   document.body.appendChild(div);
@@ -17,26 +20,31 @@ function createInterval(topE, leftE, topNumber, leftNumber) {
 
     div.style.left = `${leftR}px`;
     div.style.top = `${topR}px`;
-    if (topR >= window.innerHeight - 137) {
-      console.log(topR);
+    if (topR >= window.innerHeight - 125) {
       topNumber1 = topNumber1 * -1;
-    } else if (leftR >= window.innerWidth - 300) {
+      _randomColor = randomColor();
+      div.style.backgroundColor = `#${_randomColor}`;
+    } else if (leftR >= window.innerWidth - 120) {
       leftNumber1 = leftNumber1 * -1;
+      _randomColor = randomColor();
+      div.style.backgroundColor = `#${_randomColor}`;
     } else if (topR <= 0) {
       topNumber1 = topNumber1 * -1;
+      _randomColor = randomColor();
+      div.style.backgroundColor = `#${_randomColor}`;
     } else if (leftR <= 0) {
       leftNumber1 = leftNumber1 * -1;
+      _randomColor = randomColor();
+      div.style.backgroundColor = `#${_randomColor}`;
     }
   }, 15);
+  
 }
 
-const newElement = createInterval(400, 0, -3, 2);
-const newElement1 = createInterval(400, 550, -4, -1);
-const newElement2 = createInterval(50, 550, -3, -2);
-const newElement3 = createInterval(0, 100, 3, -3);
-const newElement4 = createInterval(300, 100, 3, -2);
-const newElement5 = createInterval(500, 400, 3, 2);
-const newElement6 = createInterval(250, 300, -5, 2);
-const newElement7 = createInterval(200, 800, 3, -2);
+const newElement = createBox(400, 20, -3, 2);
+const newElement1 = createBox(400, 643, -4, -1);
+const newElement2 = createBox(50, 242, -3, -2);
+const newElement3 = createBox(0, 900, 3, -3);
+const newElement4 = createBox(300, 143, 4, -2);
 
 // console.log(window.innerWidth, window.innerHeight);
